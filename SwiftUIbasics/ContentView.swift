512e89dd6f9dcab3beb7d2c41d040bca71b4f946
Zalo Austine <austinezaloa@gmail.com>
2021-09-22 14:12:11 +0300

    Initial Commit

---
+++ SwiftUIbasics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIbasics
+//
+//  Created by AUSTINE on 22/09/2021.
+//  Copyright © 2021 AUSTINE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIbasicsTests/SwiftUIbasicsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIbasicsTests.swift
+//  SwiftUIbasicsTests
+//
+//  Created by AUSTINE on 22/09/2021.
+//  Copyright © 2021 AUSTINE. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUIbasics
+
+class SwiftUIbasicsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
